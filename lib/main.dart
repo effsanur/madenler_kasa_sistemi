e4d261014a1d1453790e287343b31198911c89a1
Efsanur Çeliköz <efsanurcelikoz@gmail.com>
2026-05-13 15:10:53 +0300

    Firebase kuruldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Firebase çekirdek paketi
 import 'package:madenler_kasa_sistemi/app/router.dart';
 import 'package:madenler_kasa_sistemi/app/theme.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  // Flutter widget ağacının düzgün bağlandığından emin oluyoruz
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase'i başlatıyoruz.
+  // Manuel kurulum yaptığın için ek parametreye gerek duymadan
+  // google-services.json dosyasını otomatik okuyacaktır.
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
